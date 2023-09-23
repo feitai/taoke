deff400accfee216f193c99c60a9ff4cb22b1923
feitai <97171660+feitai@users.noreply.github.com>
2023-09-23 11:45:11 +0800

    Delete TestBuildPatten directory

--- TestBuildPatten/src/main/java/com/yc/bean/Test.java
+++
@@ -1,9 +0,0 @@
-package com.yc.bean;
-
-public class Test {
-    public static void main(String[] args) {
-        Person p = Person.builder().age(12).name("历史").gender("F").build();
-
-        System.out.println(p.toString());
-    }
-}
--- TestBuildPatten/src/main/java/com/yc/bean2/Test.java
+++
@@ -1,8 +0,0 @@
-package com.yc.bean2;
-
-public class Test {
-    public static void main(String[] args) {
-        Apple apple = Apple.builder().color("red").name("apple").build();
-        System.out.println(apple);
-    }
-}
